1081f8b763ef95b7685a53cb0d04df63ff85c00f
yang05051 <31024419+yang05051@users.noreply.github.com>
2020-05-08 21:21:25 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -69,7 +69,7 @@ def main():
             print('8调用成功'+str(num1)+'次')
         if req.get(r'https://api.powerbi.com/v1.0/myorg/apps',headers=headers).status_code == 200:
             num1+=1
-            print('8调用成功'+str(num1)+'次') 
+            print('8_1调用成功'+str(num1)+'次') 
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders',headers=headers).status_code == 200:
             num1+=1
             print('9调用成功'+str(num1)+'次')
