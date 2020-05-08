7e777ff7b513036cae73d4af9240684226e50fcf
yang05051 <31024419+yang05051@users.noreply.github.com>
2020-05-08 21:43:12 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -45,37 +45,37 @@ def main():
     try:
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root',headers=headers).status_code == 200:
             num1+=1
-            print("1调用成功"+str(num1)+'次')
+            print("API 1 - Success  [Total success: "+str(num1)+']')
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive',headers=headers).status_code == 200:
             num1+=1
-            print("2调用成功"+str(num1)+'次')
+            print("API 2 - Success  [Total success: "+str(num1)+']')
         if req.get(r'https://graph.microsoft.com/v1.0/drive/root',headers=headers).status_code == 200:
             num1+=1
-            print('3调用成功'+str(num1)+'次')
+            print('API 3 - Success  [Total success: '+str(num1)+']')
         if req.get(r'https://graph.microsoft.com/v1.0/users ',headers=headers).status_code == 200:
             num1+=1
-            print('4调用成功'+str(num1)+'次')
+            print('API 4 - Success  [Total success: '+str(num1)+']')
         if req.get(r'https://graph.microsoft.com/v1.0/me/messages',headers=headers).status_code == 200:
             num1+=1
-            print('5调用成功'+str(num1)+'次')    
+            print('API 5 - Success  [Total success: '+str(num1)+']')    
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
             num1+=1
-            print('6调用成功'+str(num1)+'次')    
+            print('API 6 - Success  [Total success: '+str(num1)+']')    
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
             num1+=1
-            print('7调用成功'+str(num1)+'次')
+            print('API 7 - Success  [Total success: '+str(num1)+']')
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root/children',headers=headers).status_code == 200:
             num1+=1
-            print('8调用成功'+str(num1)+'次')
+            print('API 8 - Success  [Total success: '+str(num1)+']')
         if req.get(r'https://api.powerbi.com/v1.0/myorg/apps',headers=headers).status_code == 200:
             num1+=1
-            print('8_1调用成功'+str(num1)+'次') 
+            print('API 11 - Success [Total success: '+str(num1)+']') 
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders',headers=headers).status_code == 200:
             num1+=1
-            print('9调用成功'+str(num1)+'次')
+            print('API 9 - Success  [Total success: '+str(num1)+']')
         if req.get(r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',headers=headers).status_code == 200:
             num1+=1
-            print('10调用成功'+str(num1)+'次')
+            print('API 10 - Success [Total success: '+str(num1)+']')
             print('此次运行结束时间为 :', localtime)
     except:
         print("pass")
